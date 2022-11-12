19b41ed78da66cdaf841d8b37d96223d1bc58997
SuperXu97 <1511169578@qq.com>
2022-11-12 23:13:27 +0800

    Update 4khd_crapper.py

--- 4khd_crapper/4khd_crapper.py
+++ 4khd_crapper/4khd_crapper.py
@@ -21,7 +21,6 @@ def img_crapper(url, folder_name):
         return
     
     #得到图集页数
-    url_selector = 'body > div > div > main > div.is-layout-flex.wp-container-36.wp-block-columns > div > div > div:nth-child(1) > div.is-layout-flow.wp-block-group.is-style-default > div > figure > div.page-link-box > ul > li> a'
     try:
         page_num = 1 + len(BeautifulSoup(response.text, 'lxml').select(selector = url_selector))
     except:
@@ -60,7 +59,6 @@ def img_crapper(url, folder_name):
             return
 
         #数据解析
-        img_selector = 'body > div > div > main > div > div > div > div:nth-child(1) > div.is-layout-flow.wp-block-group.is-style-default > div > figure > figure > a'
         try:
             figure_list = BeautifulSoup(response.text, 'lxml').select(selector = img_selector)
         except:
@@ -123,10 +121,6 @@ def page_crapper(page_url):
         soup = BeautifulSoup(strhtml.text, 'lxml')
     except:
         soup = BeautifulSoup(strhtml.text, 'html.parser')
-    # 爬取主页的selector
-    page_selector = 'body > div > div > div > div.is-layout-flow.wp-block-query > ul > li > div > div.is-layout-flow.wp-block-group.is-style-no-margin > figure > a'
-    # 爬取cosplay的selector
-    # pahe_selector = 'body > div > div > main > div.is-layout-constrained.entry-content.wp-block-post-content > div.is-layout-constrained.wp-container-62.wp-block-group.is-style-no-margin > div > ul > li > div > div.is-layout-flow.wp-block-group.is-style-no-margin > figure > a'
     data = soup.select(selector = page_selector)
     if data == []:
         return 0
@@ -173,13 +167,19 @@ def read_yml_all(yml_path):
     except:
         return None
 
-#配置文件初始刷
-def set_config_defaul():
+#配置文件初始设置
+def set_config_default():
     config_data_str = """\
 #图片保存的路径，（默认为.\，即与运行文件同一路径）
 save_path: .\\
-#是否请求图片保存路径的修改（true为是，默认为true）
-save_path_request: true
+#爬取界面选择，默认为主页main
+#其余选项有cosplay，album
+page_choice: main
+
+#是否请求图片保存路径的设置（true为是，默认为true）
+request_save_path: true
+#是否请求爬取界面的选择（true为是，默认为true）
+request_page_choice: true
 """
     config_data = yaml.load(config_data_str)
     f = open('./config.yml', 'a', encoding = 'utf-8')
@@ -187,6 +187,31 @@ save_path_request: true
     f.close()
 
 ### 开始主程序 ###
+
+## 全局变量设置 ##
+
+#page_url的字典（主页和分类子页）
+page_dict = {
+    'main': {
+        # 吐槽：这个网页的页面管理实在是太垃圾了！一个网页可以有多种链接...
+        'choice1': 'https://www.4khd.com/?query-3-page=',
+        'choice2': 'https://www.4khd.com/page/2?query-3-page=',
+        'choice3': 'https://www.4khd.com/?cst&query-3-page='
+    },
+    'cosplay': 'https://www.4khd.com/pages/cosplay?query-3-page=',
+    'album': 'https://www.4khd.com/pages/album?query-3-page='
+}
+
+#selector的字典
+selector_dict = {
+    'page_selector': {
+        'main': 'body > div > div > div > div.is-layout-flow.wp-block-query > ul > li > div > div.is-layout-flow.wp-block-group.is-style-no-margin > figure > a',
+        'cosplay': 'body > div > div > main > div.is-layout-constrained.entry-content.wp-block-post-content > div.is-layout-constrained.wp-container-62.wp-block-group.is-style-no-margin > div > ul > li > div > div.is-layout-flow.wp-block-group.is-style-no-margin > figure > a',
+        'album': 'body > div > div > main > div.is-layout-constrained.entry-content.wp-block-post-content > div.is-layout-constrained.wp-container-62.wp-block-group.is-style-no-margin > div > ul > li > div > div.is-layout-flow.wp-block-group.is-style-no-margin > figure > a'
+    },
+    'url_selector': 'body > div > div > main > div.is-layout-flex.wp-container-36.wp-block-columns > div > div > div:nth-child(1) > div.is-layout-flow.wp-block-group.is-style-default > div > figure > div.page-link-box > ul > li> a',
+    'img_selector': 'body > div > div > main > div > div > div > div:nth-child(1) > div.is-layout-flow.wp-block-group.is-style-default > div > figure > figure > a'
+}
 
 #请求头伪装
 if __name__ == '__main__':
@@ -199,48 +224,76 @@ yaml = ruamel.yaml.YAML()
 try:
     config_data = read_yml_all('./config.yml')
 except:
-    set_config_defaul()
+    set_config_default()
     config_data = read_yml_all('./config.yml')
-#查询是否要请求修改图片保存路径，默认为是
-#询问一次之后，之后就不再询问（可以到config.yml将save_path_request改为true来再开启一次询问）
+
+#载入保存图片的路径和所要爬取界面的设置
 try:
     save_path = config_data['save_path']
-    save_path_request = config_data['save_path_request']
+    page_choice = config_data['page_choice']
+    request_save_path = config_data['request_save_path']
+    request_page_choice = config_data['request_page_choice']
 except:
-    set_config_defaul()
+    set_config_default()
     config_data = read_yml_all('./config.yml')
     save_path = config_data['save_path']
-    save_path_request = config_data['save_path_request']
-if save_path_request:
-    config_data['save_path_request'] = False
+    page_choice = config_data['page_choice']
+    request_save_path = config_data['request_save_path']
+    request_page_choice = config_data['request_page_choice']
+#查询是否要请求修改图片保存路径，默认是
+#询问一次之后，之后就不再询问（可以到config.yml将request_save_path改为true来再开启一次询问）
+if request_save_path:
+    config_data['request_save_path'] = False
     print('请输入图片要保存到的路径（空着为保存到该运行文件所在的路径）')
     save_path = input()
-    if save_path != '':
+    if save_path == '':
+        save_path = '.\\'
+    else:
         config_data['save_path'] = save_path
     f = open('./config.yml', 'w', encoding = 'utf-8')
     yaml.dump(config_data, f)
-    save_path = config_data['save_path']
+    f.close()
 save_path += '/'
+
+#查询是否要选择爬取的界面，默认是
+#询问一次之后，之后就不再询问（可以到config.yml将request_page_choice改为true来再开启一次询问）
+if request_page_choice:
+    config_data['request_page_choice'] = False
+    print('请选择要爬取的界面[main, cosplay, album]（默认为main，即主页；输入空为默认）：')
+    page_choice = input()
+    while not page_choice in ['', 'main', 'cosplay', 'album']:
+        print('输入有误，请重新输入[main, cosplay, album]：')
+        page_choice = input()
+    if page_choice == '':
+        page_choice = 'main'
+    config_data['page_choice'] = page_choice
+    f = open('./config.yml', 'w', encoding = 'utf-8')
+    yaml.dump(config_data, f)
+    f.close()
+if page_choice == 'main':
+    origin_url = page_dict[page_choice]['choice1']
+else:
+    origin_url = page_dict[page_choice]
+
+#selector的导入
+#page_selector为界面选择器，url_selector为图集选择器，img_selector图片选择器
+page_selector = selector_dict['page_selector'][page_choice]
+url_selector = selector_dict['url_selector']
+img_selector = selector_dict['img_selector']
+
+## 爬取程序 ##
 
 #先对已储存的tiomeout的page和url的爬取
 print('对已记录的超时网页进行再爬取')
 timeout_url_crapper()
 
-#设置原始网页链接
-# cosplay原始网页
-# origin_url = 'https://www.4khd.com/pages/cosplay?query-3-page='
-# 主页原始网页
-# 吐槽：这个网页的页面管理实在是太垃圾了！一个网页可以有多种链接...
-# origin_url = 'https://www.4khd.com/page/2?query-3-page='
-# origin_url = 'https://www.4khd.com/?cst&query-3-page='
-origin_url = 'https://www.4khd.com/?query-3-page='
-
-#爬取所有页的网页
 #爬取最近更新的内容，因为有时origin_url里的内容不一定是最新的
-print('正在爬取网页https://www.4khd.com/')
-page_crapper('https://www.4khd.com/')
+#只会在爬取主页的时候运行
+if page_choice == 'main':
+    print('正在爬取网页https://www.4khd.com/')
+    page_crapper('https://www.4khd.com/')
 
-#爬取剩余的内容
+#爬取page_choice界面下所有页的网页
 i = 1
 print('正在爬取网页' + origin_url + str(i))
 while page_crapper(origin_url + str(i)) != 0: #判断是否到最后一页
